fe0e168da226daa5a6d6a85cb2a3d829eccb865a
Fredrik Jönsson <fredrikj.hem@gmail.com>
2025-09-18 12:01:51 +0200

    Private members in class

---
+++ BinVector.h
@@ -0,0 +1,23 @@
+#ifndef BIN_VECTOR
+#define BIN_VECTOR
+namespace fj71cpp {
+#include<vector>
+
+//A class for binary vectors. 
+class BinVector {
+
+public:
+
+
+private:
+std::vector<unsigned long long> mBinVector; //the storage of the bits
+int mLength; //the number of bits in the vetor
+int mLength64; //the number of 64-bit words needed to store mLength bits
+int mLastWord; //the number of bits stored in last word of the array
+unsigned long long mMask; //the last mLastWord bits are 1 rest is 0
+
+}; //class
+
+
+} //namespace
+#endif //BIN_VECTOR
